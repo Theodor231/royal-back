78aa5587c3d987116c3826852bb8e9b57326881b
Tudor Teisanu <teisanutudort@gmail.com>
2021-07-11 23:26:39 +0300

    a

---
+++ db/migrations/1626034994084-all.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class all1626034994084 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
